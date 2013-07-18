ed09d166d665ad45f61184cfddcfc24b62e37bc7
vonavi <ivvl82@gmail.com>
2013-07-18 12:43:45 +0300

    Fix typo.

--- org-protocol-jekyll.el
+++ org-protocol-jekyll.el
@@ -27,9 +27,9 @@
 ;; Emacs for editing. `org-protocol-jekyll' uses the sub-protocol
 ;; \"jekyll\" and maps URLs to local filenames defined in
 ;; `org-protocol-jekyll-alist'.
-;;
-;; For further information, see README.md found here: URL
-;; `https://github.com/vonavi/org-protocol-jekyll'.
+
+;; For further information, see README.md found here:
+;; URL `https://github.com/vonavi/org-protocol-jekyll'.
 
 ;;; Code:
 
